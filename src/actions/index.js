7949813ff9ec3890386ba59d24368fb8c2499848
RL2020 <loc.fitness1@gmail.com>
2021-11-29 14:23:31 -0500

    implemented _.memoize

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,16 +9,13 @@ export const fetchPosts = () =>  async dispatch => {
 };
 
 
-// export const fetchUser = id => async dispatch => {
-//     const response = await jsonplaceholder.get(`/users/${id}`);
-  
-//     dispatch({ type: 'FETCH_USER', payload: response.data });
-//   };
+export const fetchUser = id => dispatch => {
+    _fetchUser(id, dispatch);
+  };
 
-export const fetchUser = function(id) { 
-   return _.memoize (async function(dispatch) {
+ const _fetchUser = _.memoize(async (id, dispatch) => {
     const response = await jsonplaceholder.get(`/users/${id}`);
   
     dispatch({ type: 'FETCH_USER', payload: response.data });
-   });
-};
+
+});
