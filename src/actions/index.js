cdc2e029b23e0ab1e43b100924678de4d3df891b
RL2020 <loc.fitness1@gmail.com>
2021-11-29 14:16:11 -0500

    trying a refactor using _.memoize in actions index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,8 +15,10 @@ export const fetchPosts = () =>  async dispatch => {
 //     dispatch({ type: 'FETCH_USER', payload: response.data });
 //   };
 
-export const fetchUser = id => async dispatch => {
+export const fetchUser = function(id) { 
+   return _.memoize (async function(dispatch) {
     const response = await jsonplaceholder.get(`/users/${id}`);
   
     dispatch({ type: 'FETCH_USER', payload: response.data });
-  };
+   });
+};
